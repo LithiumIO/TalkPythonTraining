170464a4b5d60b1b153089a53e155d9764ff8677
LithiumIO <shanewasmiller@gmail.com>
2018-03-30 11:43:41 -0700

    Completed app 8

--- 08_File_Search_App/program.py
+++ 08_File_Search_App/program.py
@@ -1,4 +1,8 @@
 import os
+import collections
+
+SearchResult = collections.namedtuple('SearchResult',
+                                      'file, line, text')
 
 
 def main():
@@ -8,19 +12,26 @@ def main():
         print("Sorry we can't search that location.")
         return
 
-    text = get_folder_from_user()
+    text = get_search_text_from_user()
     if not text:
         print("We can't search for nothing silly!")
         return
 
+    matches = search_folders(folder, text)
+    match_count = 0
+    for m in matches:
+        match_count +=1
+        # print('---Match-----')
+        # print("file: " + m.file)
+        # print('line: {}'.format(m.file))
+        # print('match: ' + m.text.strip())
 
+    print("Found {:,} matches.".format(match_count))
 
 
 def print_header():
     print("------------------------------------------")
-    print("------------------------------------------")
     print("------------FILE SEARCH APP---------------")
-    print("------------------------------------------")
     print("------------------------------------------")
 
 
@@ -41,30 +52,42 @@ def get_search_text_from_user():
 
 
 def search_folders(folder, text):
-
-    all_matches = []
+    # all_matches = []
     items = os.listdir(folder)
 
     for item in items:
         full_item = os.path.join(folder, item)
         if os.path.isdir(full_item):
-            matches = search_file(full_item, text)
-            all_matches.extend(matches)
-        else:
-            matches = search_file(full_item, text)
-            all_matches.extend(matches)
+            # matches = search_file(full_item, text)
+            # all_matches.extend(matches)
+            # for m in matches:
+            #    yield m
+            yield from search_folders(full_item, text)
 
-    return all_matches
+        else:
+            yield from search_file(full_item, text)
+            # matches = search_file(full_item, text)
+            # all_matches.extend(matches)
+            # for m in matches:
+            #    yield m
+
+    # return all_matches
 
 
 def search_file(filename, search_text):
-    matches = []
+    # matches = []
     with open(filename, 'r', encoding='utf-8') as fin:
 
+        line_num = 0
         for line in fin:
+            line_num += 1
             if line.lower().find(search_text) >= 0:
+                m = SearchResult(line=line_num, file=filename, text=line)
+                # matches.append(m)
+                yield m
 
-        return matches
+
+        #return matches
 
 if __name__ == '__main__':
     main()
